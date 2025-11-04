97a17c69bac79bbfcfd4bcee9c2a272c2c122136
JoseReynaFideson <jose.reynasonora.gob.mx>
2025-11-04 10:16:34 -0700

    agg dia a messagelist s

--- admin-whatsapp/src/components/messages/ConversationList.jsx
+++ admin-whatsapp/src/components/messages/ConversationList.jsx
@@ -106,7 +106,6 @@ export default function ConversationList({ conversations = [], onSelect, selecte
             }
           }
 
-
           return (
             <div
               key={conv.id}
